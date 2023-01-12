2bb775945dd74a07d5ef47769f8c0ae94811cd66
seojun-kim <80928795+seojun-kim@users.noreply.github.com>
2023-01-12 15:09:05 +0900

    seojunBack

---
+++ src/main/java/kr/hs/dgsw/second_hand/SecondHandApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.second_hand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondHandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hs/dgsw/second_hand/SecondHandApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.second_hand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
